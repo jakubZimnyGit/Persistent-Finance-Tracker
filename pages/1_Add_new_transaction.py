a58fa42881785f2da15db55969880b46242f3b31
Jakub Zimny <jakub.zimny.gdansk@gmail.com>
2026-02-14 20:46:27 +0100

    feat: implement Add Transaction page with Streamlit inputs and CRUD integration

--- pages/1_Add_new_transaction.py
+++ pages/1_Add_new_transaction.py
@@ -1,10 +1,16 @@
 import streamlit as st
+from app.crud import create_transaction
+from app.config import CATEGORIES
 
-amount = st.number_input("Amount", min_value=0.0, step=0.01)
+
+amount = st.number_input("Amount", min_value=0.0, step=0.01, value=None)
 description = st.text_input("short description", max_chars=65, help="Provide a brief description of the transaction.")
-category = st.selectbox("Category", options=["Food", "Transport", "Entertainment",
-                                            "Income", "Investment", "Savings",  "Health",
-                                            "Education", "Utilities", "Personal Care",
-                                            "Gifts & Donations", "Travel", "Subscriptions", "Other"])
+category = st.selectbox("Category", options=CATEGORIES)
 
-st.button("Add Transaction", on_click=(lambda: st.success("Transaction added successfully!")))
+if st.button("Add Transaction"):
+    transaction = create_transaction(amount, description, category)
+    if transaction:
+        st.success("Transaction added successfully!")
+    else:
+        st.error("Failed to add transaction. Please check your input.")
+
