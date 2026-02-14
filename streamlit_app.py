8c67fb6ba309ceef1705905e54a606318f0b374e
Jakub Zimny <jakub.zimny.gdansk@gmail.com>
2026-02-14 20:41:23 +0100

    feat: init database at startup

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,8 @@
 import streamlit as st
+from app.database import init_db
 
+if __name__ == "__main__":
+    init_db()
+
+    st.title("Persistent Finance Tracker")
+    st.write("Welcome to the Persistent Finance Tracker! Use the sidebar to navigate through different features.")
