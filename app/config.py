d97339ab76abb2a907669ea5f4abfee009c9fc69
Jakub Zimny <jakub.zimny.gdansk@gmail.com>
2026-02-14 20:32:33 +0100

    refactor: move constants and db settings to central config

---
+++ app/config.py
@@ -0,0 +1,5 @@
+SQLALCHEMY_DATABASE_URL = "sqlite:///finance.db"
+CATEGORIES = ["Food", "Transport", "Entertainment",
+              "Income", "Investment", "Savings",  "Health",
+              "Education", "Utilities", "Personal Care",
+              "Gifts & Donations", "Travel", "Subscriptions", "Other"]
