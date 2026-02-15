02a1562b060ea6eb67613858461eb3af2b9cf8a7
Jakub Zimny <jakub.zimny.gdansk@gmail.com>
2026-02-15 15:30:04 +0100

    feat: implement transaction management with editing and deletion in sidebar

--- pages/2_List_of_transactions.py
+++ pages/2_List_of_transactions.py
@@ -1,14 +1,68 @@
+from click import edit
+from narwhals import col
+from numpy import delete
 import streamlit as st
 import pandas as pd
-from app.crud import get_all_transactions
+from app.crud import get_all_transactions, update_transaction_amount, delete_transaction
 
 st.title("List of Transactions")
 transactions = get_all_transactions()
 if transactions:
     df = pd.DataFrame([{
+        "ID": t.id,
         "Amount": t.amount,
         "Description": t.description,
         "Category": t.category,
-        "Date": t.date.strftime("%Y-%m-%d")
+        "Date": t.date.strftime("%Y-%m-%d"),
     } for t in transactions])
-    st.dataframe(df)
+    
+
+edited_df = st.data_editor(
+    df, 
+    disabled=["ID", "Description", "Category", "Date"],
+    hide_index=True,
+    key="my_editor"
+)
+
+st.sidebar.title("Manage Transactions")
+with st.sidebar:
+    st.subheader("Delete Transaction")
+    to_delete = st.selectbox(
+            "Select to remove:",
+            options=transactions,
+            format_func=lambda x: f"{x.description} ({x.amount})"
+        )
+
+        # Logika potwierdzenia (Twoje Yes/No)
+    if st.button("Delete Selected", type="primary", use_container_width=True):
+        st.session_state["confirm_delete"] = True
+    if st.session_state.get("confirm_delete"):
+        st.error(f"Do you really want to delete this transaction?")
+        col_yes, col_no = st.columns(2)
+        
+        with col_yes:
+            if st.button("Yes", key="confirm_yes", use_container_width=True):
+                delete_transaction(to_delete.id)
+                st.session_state["confirm_delete"] = False
+                st.rerun()
+        
+        with col_no:
+            if st.button("No", key="confirm_no", use_container_width=True):
+                st.session_state["confirm_delete"] = False
+                st.rerun()
+
+    changes = st.session_state["my_editor"]["edited_rows"]
+    if changes:
+        st.write("Wykryto zmiany:")
+        if st.button("Zapisz zmiany w bazie"):
+            for row_index, updated_values in changes.items():
+                trans_id = int(df.iloc[int(row_index)]["ID"])
+                new_amount = updated_values["Amount"]
+                update_transaction_amount(trans_id, new_amount)
+                st.success(f"Zaktualizowano transakcję ID {trans_id} na kwotę {new_amount}")
+
+            st.rerun()
+        st.success("Changes saved!")
+
+st.divider()
+
